d4022da6e9514ea07e76491c0d540897ed54d01e
michaelodonnell18 <michaelodonnell18@gmail.com>
2022-02-09 12:53:27 -0500

    described file to parse and added parser to app

---
+++ fileparser.js
@@ -0,0 +1,7 @@
+const salaries = "https://docs.google.com/spreadsheets/d/19h0WT9Xjf9ncNmJDWpd8fzz7YnB37nemDnxKSO0EE8o/edit#gid=1664289965";
+Papa.parse(salaries, {
+	download: true,
+	complete: function(results) {
+		console.log(results);
+	}
+});
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+/*class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello {this.props.name}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <HelloMessage name="" />,
+  document.getElementById('hello-example')
+);
+*/
+export default App;
